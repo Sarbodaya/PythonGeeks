51ca187fb497176217ffc52e31378b303817c17f
SARBODAYA JENA <54230020+Sarbodaya@users.noreply.github.com>
2020-11-01 12:42:54 +0530

    Add files via upload

---
+++ Data Types/AcessingElementsFromList.py
@@ -0,0 +1,15 @@
+List = ["Geeks", "For", "Geeks"]
+
+print("Accessing elements from the list : ")
+print(List[0])
+print(List[1])
+
+# Creating a Multi Dimensional List
+# By Nesting a list inside a list
+List = [["Sarbodaya", "Jena"], ["Indian Air Force", "Indian Navy", "Indian Army"]]
+print(List)
+
+# Accessing a element from Multi Dimensional List
+print("Accessing a element from Multi Dimensional List")
+print(List[0][1])
+print(List[1][2])
