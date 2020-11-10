065ed6727e0470e6f80dab77b82dbda817004a60
SARBODAYA JENA <54230020+Sarbodaya@users.noreply.github.com>
2020-11-10 18:10:30 +0530

    Add files via upload

---
+++ Operator/Arithmetic.py
@@ -0,0 +1,33 @@
+# Examples of Arithmetic Operator
+a = 9
+b = 4
+
+# Addition of numbers
+add = a + b
+
+# Subtraction of numbers
+sub = a - b
+
+# Multiplication of number
+mul = a * b
+
+# Division(float) of number
+div1 = a / b
+
+# Division(floor) of number
+div2 = a // b
+
+# Modulo of both number
+mod = a % b
+
+# Power
+p = a ** b
+
+# print results
+print("ADD : ", add)
+print("SUB : ", sub)
+print("MUL : ", mul)
+print("DIV1 : ", div1)
+print("DIV2 : ", div2)
+print("MOD : ", mod)
+print("POWER : ", p)
