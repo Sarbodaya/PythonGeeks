8cf81aa42d0d32c2f8c72051e6072459a339723d
SARBODAYA JENA <54230020+Sarbodaya@users.noreply.github.com>
2020-11-10 18:53:54 +0530

    Ternary Operators Blogs Updated

---
+++ Operator/LogicalAndBitWiseNotOperator.py
@@ -0,0 +1,16 @@
+# A Python program that uses Logical Not or ! on boolean
+a = not True
+b = not False
+print(a)
+print(b)
+# Output : False
+# Output : True
+
+# A Python program that uses Bitwise Not or ~ on boolean
+a = True
+b = False
+print(~a)
+print(~b)
+
+# Reason: The bitwise not operator ~ returns the complement of a number i.e.,
+# it switches each 1 to 0 and each 0 to 1. Booleans True and False have values 1 and 0 respectively.
